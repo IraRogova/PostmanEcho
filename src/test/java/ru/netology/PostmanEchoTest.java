5e92926e87668eb001e83153bde4671364f829ee
Irina Rogova <buyersbirds@gmail.com>
2021-01-19 11:17:33 +0600

    created new repo

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnPostBody () {
+
+
+}
